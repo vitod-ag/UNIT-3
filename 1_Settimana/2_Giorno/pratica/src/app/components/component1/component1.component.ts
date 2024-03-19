33da49f3f8566b850caf6d2ded6279d90965fd70
vitod-ag <116908757+vitod-ag@users.noreply.github.com>
2024-03-19 16:10:01 +0100

    esercizio UNIT3 settimana1 giorno2

---
+++ 1_Settimana/2_Giorno/pratica/src/app/components/component1/component1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.scss']
+})
+export class Component1Component {
+  title = 'ciao comp1';
+  subtitle = 'comp1 works!'
+}
---
+++ 1_Settimana/2_Giorno/pratica/src/app/components/component2/component2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component2',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.scss']
+})
+export class Component2Component {
+  title = 'ciao comp2';
+  subtitle = 'comp2 works!'
+}
