05b587f1da2fa5e86b8722f18e33f8aa868539c7
vitod-ag <116908757+vitod-ag@users.noreply.github.com>
2024-03-19 13:04:58 +0100

    commit UNIT3 settimana1 giorno2 lezione

--- 1_Settimana/2_Giorno/angular/src/app/app.component.ts
+++ 1_Settimana/2_Giorno/angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular';
+    title = '*Angular*';
+    content = 'UNIT3 settimana1 giorno2 lezione'
 }
