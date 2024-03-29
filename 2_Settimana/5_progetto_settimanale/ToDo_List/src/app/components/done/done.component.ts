8d96c2ac02248f887735a1984847d1218dd50ef1
vitod-ag <116908757+vitod-ag@users.noreply.github.com>
2024-03-29 18:47:04 +0100

    commit popolato done e undone progetto settimanale UNIT3

--- 2_Settimana/5_progetto_settimanale/ToDo_List/src/app/components/done/done.component.ts
+++ 2_Settimana/5_progetto_settimanale/ToDo_List/src/app/components/done/done.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/interfaces/todo.interface';
+import { User } from 'src/app/interfaces/user.interface';
+import { TodoService } from 'src/app/services/todo.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-done',
   templateUrl: './done.component.html',
   styleUrls: ['./done.component.scss']
 })
-export class DoneComponent {
+export class DoneComponent implements OnInit {
 
-}
+  todos: Todo[] = [];
+  users: User[] = [];
+
+  constructor(private srvToDo: TodoService, private srvUser: UserService) {}
+
+    ngOnInit(): void {
+      this.addToDoList();
+      this.addUser();
+    }
+  
+    async addToDoList() {
+      try {
+        this.todos = await this.srvToDo.getTodos();
+      }catch (error) {
+        console.error('Error fetching ToDoList:', error);
+      }
+    }
+  
+    async addUser() {
+      try {
+        this.users = await this.srvUser.getUsers();
+      }catch (error) {
+        console.error('Error fetching User:', error);
+      }
+    }
+
+    getName(id:number) {
+      let name = ''
+      this.users.forEach((value) => {
+        if(value.id === id) {
+          name = `${value.firstName} ${value.lastName}`
+        }
+      })
+      return name;
+    }
+
+    getDone(): Todo[] {
+      return this.todos.filter(todo => todo.completed)
+    }
+  }
+
+
--- 2_Settimana/5_progetto_settimanale/ToDo_List/src/app/components/undone/undone.component.ts
+++ 2_Settimana/5_progetto_settimanale/ToDo_List/src/app/components/undone/undone.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/interfaces/todo.interface';
+import { User } from 'src/app/interfaces/user.interface';
+import { TodoService } from 'src/app/services/todo.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-undone',
   templateUrl: './undone.component.html',
   styleUrls: ['./undone.component.scss']
 })
-export class UndoneComponent {
+export class UndoneComponent implements OnInit {
+  todos: Todo[] = [];
+  users: User[] = [];
 
-}
+  constructor(private srvToDo: TodoService, private srvUser: UserService) {}
+
+    ngOnInit(): void {
+      this.addToDoList();
+      this.addUser();
+    }
+  
+    async addToDoList() {
+      try {
+        this.todos = await this.srvToDo.getTodos();
+      }catch (error) {
+        console.error('Error fetching ToDoList:', error);
+      }
+    }
+  
+    async addUser() {
+      try {
+        this.users = await this.srvUser.getUsers();
+      }catch (error) {
+        console.error('Error fetching User:', error);
+      }
+    }
+
+    getName(id:number) {
+      let name = ''
+      this.users.forEach((value) => {
+        if(value.id === id) {
+          name = `${value.firstName} ${value.lastName}`
+        }
+      })
+      return name;
+    }
+
+    getUndone(): Todo[] {
+      return this.todos.filter(todo => !todo.completed)
+    }
+  }
+
